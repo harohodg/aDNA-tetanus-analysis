e29188e24c58f3c35a1d1e98f2603f5e060d64b9
Harold Hodgins <hhodgins@uwaterloo.ca>
2021-10-20 15:10:28 -0400

    Updated bwa launching code
    Now checks the input and contig files.

--- launch_bwa_align_samse_jobs.sh
+++ launch_bwa_align_samse_jobs.sh
@@ -8,6 +8,14 @@
 #Should be updated to mirror input folder structure under output folder
 #Can still assume that the last folder name is the corresponding contigs folder name
 
+#Revisions:
+#October 20, 2021
+# - inputFolders checked to make sure they have only one *.fq file in them
+# - contigs folder checked to see if there is a corresponding sraID folder
+# - contigs/sraID folder checked to see if there is only one *.fa file in them
+# Based off code from https://www.ducea.com/2009/03/05/bash-tips-if-e-wildcard-file-check-too-many-arguments/
+
+
 if [ "$#" -ne 7 ]; then
     echo "Incorrect # of arguments."
     echo "Usage : ./launch_bwa_align_samse_jobs.sh <n> <o> <l> <contigs_folder> <input_folder> <output_folder> <account>"
@@ -36,14 +44,24 @@ maybeRun=$(find "$inputFolder" -mindepth 2 -maxdepth 2 -type f -name "*.fq" -not
 for sraID in $( comm -23 <(echo "$maybeRun") <(echo "$doNotRun") )
 do
     #echo "$sraID"
-    inputFile=$(ls "$inputFolder"/"$sraID"/*.fq)
-    contigs=$(ls "$contigsFolder"/"$sraID"/*.fa)
-    outputFile="$outputFolder"/"$sraID"/$(basename "$inputFile" | sed 's/.fq//').mappedTo.$(basename "$contigs" | sed 's/.fa//').sorted.bam
-    
-    inputSize=$(du -BM "$inputFile" | cut -f1 -d'M')
-    jobTime=0:0:"$(expr $inputSize \* 2)"
-    
-    echo sbatch --account="$account" --time="$jobTime" --job-name="$sraID"_"$jobNameFormat" --output=%x-%j.out bwa_align_samse_job.sh "$n" "$o" "$l" "$contigs" "$inputFile" "$outputFile"
-    
+    inputFiles=$(ls  "$inputFolder"/"$sraID"/*.fq 2> /dev/null | wc -l)
+    contigFiles=$(ls "$contigsFolder"/"$sraID"/*.fa 2> /dev/null | wc -l)
+    if [ "$inputFiles" != "1" ]
+    then
+        echo "Incorrect number of input files found in $inputFolder/$sraID"
+    else
+        if [ "$contigFiles" != "1" ]
+        then
+            echo "Incorrect number of contig files found in $contigsFolder/$sraID"
+        else
+            inputFile=$(ls "$inputFolder"/"$sraID"/*.fq)
+            contigs=$(ls "$contigsFolder"/"$sraID"/*.fa)
+            outputFile="$outputFolder"/"$sraID"/$(basename "$inputFile" | sed 's/.fq//').mappedTo.$(basename "$contigs" | sed 's/.fa//').sorted.bam
+            
+            inputSize=$(du -BM "$inputFile" | cut -f1 -d'M')
+            jobTime=0:0:"$(expr $inputSize \* 2)"
+            
+            sbatch --account="$account" --time="$jobTime" --job-name="$sraID"_"$jobNameFormat" --output=%x-%j.out bwa_align_samse_job.sh "$n" "$o" "$l" "$contigs" "$inputFile" "$outputFile"
+        fi
+    fi
 done
-
